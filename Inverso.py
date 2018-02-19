d093ae4525d6730734a89069c23565d91c8c6787
paula1421 <36423833+paula1421@users.noreply.github.com>
2018-02-19 17:54:32 -0500

    Update Inverso.py

--- Inverso.py
+++ Inverso.py
@@ -2,11 +2,10 @@ def longitud(num):
 	if num > 0:
 		return longitud(num/10) + 1
 	return 0
+
 def invertir(num):
     if(num > 1):
         return  invertir(num/10) + (num%10)* pow(10,longitud(num/10))
     return 0
 
 print invertir(input("Ingrese un numero "))
-
-
