aae2263a4af248c39759a8b50497bb97d91b1bd2
paula1421 <36423833+paula1421@users.noreply.github.com>
2018-02-19 17:52:52 -0500

    Update NumeroMayor.py

--- NumeroMayor.py
+++ NumeroMayor.py
@@ -2,6 +2,7 @@ def longitud(num):
 	if num > 0:
 		return longitud(num/10) + 1
 	return 0
+
 def invertir(num):
     if(num > 1):
         return  invertir(num/10) + (num%10)* pow(10,longitud(num/10))
@@ -14,4 +15,5 @@ def mayor(num):
         else:
             return mayor(invertir(num)/10)
     return num
+
 print mayor(input("Ingrese un numero "))
