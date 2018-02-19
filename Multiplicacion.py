16ffb64b14509d734867071baa601e93c3402429
Camiloqui <camiloquirogam@outlook.com>
2018-02-19 17:43:35 -0500

    Update Multiplicacion.py

--- Multiplicacion.py
+++ Multiplicacion.py
@@ -2,5 +2,4 @@ def multiplicacion(adicion, variable):
 	if variable > 1:
 		return adicion + multiplicacion(adicion, variable-1)
 	return adicion
-
 print multiplicacion(input("Ingrese un numero "), input("Ingrese un numero "))
