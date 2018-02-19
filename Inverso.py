21f758296c8cc55af7c4f49649bd0c31805bc688
Camiloqui <camiloquirogam@outlook.com>
2018-02-19 17:43:06 -0500

    Update Inverso.py

--- Inverso.py
+++ Inverso.py
@@ -2,7 +2,6 @@ def longitud(num):
 	if num > 0:
 		return longitud(num/10) + 1
 	return 0
-
 def invertir(num):
     if(num > 1):
         return  invertir(num/10) + (num%10)* pow(10,longitud(num/10))
