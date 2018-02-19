15cf9dbcd08ad78913307705ab9c698db067001f
paula1421 <36423833+paula1421@users.noreply.github.com>
2018-02-19 17:49:11 -0500

    Update Potencia.py

--- Potencia.py
+++ Potencia.py
@@ -2,6 +2,4 @@ def potencia(base, exponente):
 	if exponente > 1:
 		return base * potenciaPrueba(base, exponente-1)
 	return base
-
-
 print potencia(input("Ingrese la base "), input("Ingrese el exponente "))
