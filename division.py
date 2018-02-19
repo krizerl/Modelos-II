6e3db738d65d16e11280730aab5b833d11a7f3a3
paula1421 <36423833+paula1421@users.noreply.github.com>
2018-02-19 17:50:24 -0500

    Update division.py

--- division.py
+++ division.py
@@ -2,6 +2,4 @@ def division(dividendo, divisor):
 	if dividendo >= divisor:
 		return division(dividendo - divisor, divisor)+1
 	return 0
-
-
 print division(input("Ingrese el dividendo "), input("Ingrese el divisor "))
