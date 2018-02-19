c6c44eeb706c4db5e5667d802be07d397f1d05fd
paula1421 <36423833+paula1421@users.noreply.github.com>
2018-02-19 17:53:55 -0500

    Update Multiplicacion.py

--- Multiplicacion.py
+++ Multiplicacion.py
@@ -2,4 +2,5 @@ def multiplicacion(adicion, variable):
 	if variable > 1:
 		return adicion + multiplicacion(adicion, variable-1)
 	return adicion
+
 print multiplicacion(input("Ingrese un numero "), input("Ingrese un numero "))
