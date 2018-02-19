045672bda14aad88975d2f88c05503df583f76dd
krizerl <krizerl90@gmail.com>
2018-02-19 17:15:53 -0500

    Add files via upload
    :3

---
+++ Inverso.py
@@ -0,0 +1,13 @@
+def longitud(num):
+	if num > 0:
+		return longitud(num/10) + 1
+	return 0
+
+def invertir(num):
+    if(num > 1):
+        return  invertir(num/10) + (num%10)* pow(10,longitud(num/10))
+    return 0
+
+print invertir(input("Ingrese un numero "))
+
+
